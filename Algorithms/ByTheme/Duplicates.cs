eedb97d2ab74b53de0defec13a44abd64cf70112
Hervé Blondeau <herve@projet-nu.fr>
2020-04-09 09:36:43 +0200

    Add find first non duplicate

--- Algorithms/ByTheme/Duplicates.cs
+++ Algorithms/ByTheme/Duplicates.cs
@@ -18,4 +18,31 @@ public class Duplicates
 
         throw new KeyNotFoundException();
     }
+
+    public T FindFirstNonDuplicate<T>(T[] elements)
+    {
+        Dictionary<T, int> nbOccurrences = new Dictionary<T, int>();
+
+        for (int i = 0; i < elements.Length; i++)
+        {
+            if (nbOccurrences.ContainsKey(elements[i]))
+            {
+                nbOccurrences[elements[i]]++;
+            }
+            else
+            {
+                nbOccurrences.Add(elements[i], 1);
+            }
+        }
+
+        for (int i = 0; i < elements.Length; i++)
+        {
+            if (nbOccurrences[elements[i]] == 1)
+            {
+                return elements[i];
+            }
+        }
+
+        throw new KeyNotFoundException();
+    }
 }
--- AlgorithmsTests/DuplicatesTests.cs
+++ AlgorithmsTests/DuplicatesTests.cs
@@ -16,10 +16,10 @@ namespace AlgorithmsTests
         public void FindFirstDuplicate_DuplicateInt_PerformsCorrectly(int[] elements, int expected)
         {
             // Arrange
-            Duplicates firstDuplicate = new Duplicates();
+            Duplicates duplicates = new Duplicates();
 
             // Act
-            var actual = firstDuplicate.FindFirstDuplicate(elements);
+            var actual = duplicates.FindFirstDuplicate(elements);
 
             // Assert
             Assert.AreEqual(expected, actual);
@@ -37,8 +37,40 @@ namespace AlgorithmsTests
 
             // Act
             var actual = firstDuplicate.FindFirstDuplicate(elements);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 1, 2 }, 2)]
+        [DataRow(new int[] { 1, 2, 2 }, 1)]
+        [DataRow(new int[] { 6, 3, 2, 3, 6 }, 2)]
+        [DataRow(new int[] { 8, 8, 2, 2, 5, 4, 4, 3, 7, 7, 7 }, 5)]
+        [DataRow(new int[] { 8, 7, 2, 5, 4, 2, 4, 3, 8, 7, 7 }, 5)]
+        [DataRow(new int[] { 1, 1, 1, 2, 3, 3, 3, 4, 5, 5, 5, 6 }, 2)]
+        [DataRow(new int[] { 1, 2, 3, 2, 1, 4 }, 3)]
+        public void FindFirstNonDuplicate_NonDuplicateInt_PerformsCorrectly(int[] elements, int expected)
+        {
+            // Arrange
+            Duplicates duplicates = new Duplicates();
+
+            // Act
+            var actual = duplicates.FindFirstNonDuplicate(elements);
 
             // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 1 })]
+        [DataRow(new int[] { 1, 1, 2, 2 })]
+        [DataRow(new int[] { 1, 2, 3, 1, 2, 3, 1, 2, 3})]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void FindFirstNonDuplicate_NoNonDuplicateInt_ThrowsException(int[] elements)
+        {
+            // Arrange
+            Duplicates duplicates = new Duplicates();
+
+            // Act
+            var actual = duplicates.FindFirstNonDuplicate(elements);
         }
     }
 }
