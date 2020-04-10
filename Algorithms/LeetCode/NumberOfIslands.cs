a6439cf67f7e7ef6aa812b784b46d2be4f2b53d2
Hervé Blondeau <herve@projet-nu.fr>
2020-04-10 13:38:27 +0200

    Add number of islands

---
+++ Algorithms/LeetCode/NumberOfIslands.cs
@@ -0,0 +1,47 @@
+﻿// https://leetcode.com/problems/number-of-islands/
+namespace Algorithms.LeetCode
+{
+    public class NumberOfIslands
+    {
+        private int _width;
+        private int _height;
+
+        public int CountIslands(int[,] lands)
+        {
+            _width = lands.GetLength(0);
+            _height = lands.GetLength(1);
+
+            int nbIslands = 0;
+
+            for (int i = 0; i < _width; i++)
+            {
+                for (int j = 0; j < _height; j++)
+                {
+                    if (lands[i, j] == 1)
+                    {
+                        _processLand(i, j, lands);
+                        nbIslands++;
+                    }
+                }
+            }
+
+            return nbIslands;
+        }
+
+        private void _processLand(int x, int y, int[,] lands)
+        {
+            if (x < 0 || x >= _width || y < 0 || y >= _height || lands[x, y] == 0)
+            {
+                return;
+            }
+
+            lands[x, y] = 0;
+
+            _processLand(x - 1, y, lands);
+            _processLand(x + 1, y, lands);
+            _processLand(x, y - 1, lands);
+            _processLand(x, y + 1, lands);
+
+        }
+    }
+}
---
+++ AlgorithmsTests/NumberOfIslandsTests.cs
@@ -0,0 +1,96 @@
+using Algorithms.LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsTests
+{
+    [TestClass]
+    public class NumberOfIslandsTests
+    {
+
+        [TestMethod]
+        public void CountIslands_OneIsland_Returns1()
+        {
+            // Arrange
+            int[,] spots = new int[,]
+            {
+                { 1,1,1,1,0 },
+                { 1,1,0,1,0 },
+                { 1,1,0,0,0 },
+                { 0,0,0,0,0 }
+            };
+            NumberOfIslands numberOfIslandsSolver = new NumberOfIslands();
+
+            // Act
+            var actual = numberOfIslandsSolver.CountIslands(spots);
+
+            // Assert
+            Assert.AreEqual(1, actual);
+        }
+
+        [TestMethod]
+        public void CountIslands_TwoIslands_Returns2()
+        {
+            // Arrange
+            int[,] spots = new int[,]
+            {
+                { 1, 1, 0, 0, 0 },
+                { 1, 1, 0, 0, 0 },
+                { 0, 0, 1, 1, 0 },
+                { 0, 0, 0, 1, 1 }
+            };
+            NumberOfIslands numberOfIslandsSolver = new NumberOfIslands();
+
+            // Act
+            var actual = numberOfIslandsSolver.CountIslands(spots);
+
+            // Assert
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod]
+        public void CountIslands_ThreeIslands_Returns3()
+        {
+            // Arrange
+            int[,] spots = new int[,]
+            {
+                { 1, 1, 0, 0, 0 },
+                { 1, 1, 0, 0, 0 },
+                { 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 1, 1 }
+            };
+            NumberOfIslands numberOfIslandsSolver = new NumberOfIslands();
+
+            // Act
+            var actual = numberOfIslandsSolver.CountIslands(spots);
+
+            // Assert
+            Assert.AreEqual(3, actual);
+        }
+
+        [TestMethod]
+        public void CountIslands_ElevenIslands_Returns11()
+        {
+            // Arrange
+            int[,] spots = new int[,]
+            {
+                { 0, 0, 0, 0, 0, 0, 1, 1, 0, 1 },
+                { 0, 1, 0, 1, 1, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 1, 0, 0, 1, 1, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 1, 1, 0, 0, 1, 0, 1 },
+                { 1, 1, 0, 0, 1, 0, 0, 1, 0, 1 },
+                { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0 },
+                { 0, 0, 1, 0, 1, 0, 1, 0, 0, 0 },
+                { 0, 1, 1, 1, 1, 0, 0, 1, 1, 1 },
+                { 0, 0, 1, 0, 0, 0, 0, 0, 1, 1 },
+            };
+            NumberOfIslands numberOfIslandsSolver = new NumberOfIslands();
+
+            // Act
+            var actual = numberOfIslandsSolver.CountIslands(spots);
+
+            // Assert
+            Assert.AreEqual(11, actual);
+        }
+    }
+}
