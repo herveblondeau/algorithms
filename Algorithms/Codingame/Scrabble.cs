80565dbc46ff31b53a0a91f30f7b089b87fa9788
Hervé Blondeau <herve@projet-nu.fr>
2020-04-10 15:58:16 +0200

    Add Codingame's Scrabble

---
+++ Algorithms/Codingame/Scrabble.cs
@@ -0,0 +1,101 @@
+﻿// https://www.codingame.com/training/medium/scrabble
+
+using System.Collections.Generic;
+
+public class Scrabble
+{
+
+    public string FindBestWord(string[] words, string letters)
+    {
+        string bestWord = string.Empty;
+        int bestScore = 0;
+
+        int currentScore;
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (_isPlayable(words[i], letters))
+            {
+                currentScore = _getScore(words[i]);
+                if (currentScore > bestScore)
+                {
+                    bestScore = currentScore;
+                    bestWord = words[i];
+                }
+            }
+        }
+
+        return bestWord;
+    }
+
+    private bool _isPlayable(string word, string letters)
+    {
+        Dictionary<char, int> letterFrequencies = _getLetterFrequencies(letters);
+        foreach (char c in word)
+        {
+            if (!letterFrequencies.ContainsKey(c) || letterFrequencies[c] == 0)
+            {
+                return false;
+            }
+            letterFrequencies[c]--;
+        }
+        return true;
+    }
+
+    private Dictionary<char, int> _getLetterFrequencies(string letters)
+    {
+        Dictionary<char, int> frequencies = new Dictionary<char, int>();
+        foreach (char c in letters)
+        {
+            if (frequencies.ContainsKey(c))
+            {
+                frequencies[c]++;
+            }
+            else
+            {
+                frequencies.Add(c, 1);
+            }
+        }
+        return frequencies;
+    }
+
+    private int _getScore(string word)
+    {
+        int score = 0;
+        foreach (char c in word)
+        {
+            score += _letterScores[c];
+        }
+        return score;
+    }
+
+    private Dictionary<char, int> _letterScores = new Dictionary<char, int>
+    {
+        { 'a', 1 },
+        { 'b', 3 },
+        { 'c', 3 },
+        { 'd', 2 },
+        { 'e', 1 },
+        { 'f', 4 },
+        { 'g', 2 },
+        { 'h', 4 },
+        { 'i', 1 },
+        { 'j', 8 },
+        { 'k', 5 },
+        { 'l', 1 },
+        { 'm', 3 },
+        { 'n', 1 },
+        { 'o', 1 },
+        { 'p', 3 },
+        { 'q', 10 },
+        { 'r', 1 },
+        { 's', 1 },
+        { 't', 1 },
+        { 'u', 1 },
+        { 'v', 4 },
+        { 'w', 4 },
+        { 'x', 8 },
+        { 'y', 4 },
+        { 'z', 10 },
+    };
+
+}
---
+++ AlgorithmsTests/ScrabbleTests.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsTests
+{
+    [TestClass]
+    public class ScrabbleTests
+    {
+
+        [TestMethod]
+        [DataRow(new string[] { "because", "first", "these", "could", "which" }, "hicquwh", "which")]
+        [DataRow(new string[] { "some", "first", "potsie", "day", "could", "postie", "from", "have", "back", "this" }, "sopitez", "potsie")]
+        [DataRow(new string[] { "after", "repots", "user", "powers", "these", "time", "know", "from", "could", "people" }, "tsropwe", "powers")]
+        [DataRow(new string[] { "after", "repots", "poowers", "powers", "these", "time", "know", "from", "could", "people" }, "tsropwe", "powers")]
+        [DataRow(new string[] { "qzyoq", "azejuy", "kqjsdh", "aeiou", "qsjkdh" }, "qzaeiou", "aeiou")]
+        [DataRow(new string[] { "arrest", "rarest", "raster", "raters", "sartre", "starer", "waster", "waters", "wrest", "wrase" }, "arwtsre", "waster")]
+        //[DataRow(new string[] , "", "")]
+        public void FindBestWord_WhenCalled_PerformsCorrectly(string[] words, string letters, string expected)
+        {
+            // Arrange
+            Scrabble scrabbleSolver = new Scrabble();
+
+            // Act
+            var actual = scrabbleSolver.FindBestWord(words, letters);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
