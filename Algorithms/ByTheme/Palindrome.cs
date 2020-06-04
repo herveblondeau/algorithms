d67cb656f8fa05095e5c37ff75fb1aa989b63b18
Hervé Blondeau <herve@projet-nu.fr>
2020-06-04 09:22:54 +0200

    Add Palindrome

---
+++ Algorithms/ByTheme/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.ByTheme
+{
+    public class Palindrome
+    {
+        public static bool IsPalindromePermutation(string input)
+        {
+            Dictionary<char, int> nbOccurrencesPerCharacter = new Dictionary<char, int>();
+            foreach (char c in input)
+            {
+                if (char.IsWhiteSpace(c))
+                {
+                    continue;
+                }
+
+                if (nbOccurrencesPerCharacter.ContainsKey(c))
+                {
+                    nbOccurrencesPerCharacter[c]++;
+                }
+                else
+                {
+                    nbOccurrencesPerCharacter.Add(c, 1);
+                }
+            };
+
+            int nbOddCharacters = nbOccurrencesPerCharacter.Count(pair => pair.Value % 2 == 1);
+            return nbOddCharacters <= 1;
+        }
+    }
+}
---
+++ AlgorithmsTests/PalindromeTests.cs
@@ -0,0 +1,55 @@
+using Algorithms.ByTheme;
+using BenderRobot;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmsTests
+{
+    [TestClass]
+    public class PalindromeTests
+    {
+        [TestMethod]
+        [DataRow("a")]
+        [DataRow("aa")]
+        [DataRow("aaa")]
+        [DataRow("a aa")]
+        [DataRow("aba")]
+        [DataRow("abba")]
+        [DataRow("abcba")]
+        [DataRow("abccba")]
+        [DataRow("abc cba")]
+        [DataRow("  a bc c b  a")]
+        [DataRow(" c a bc  b c a")]
+        [DataRow("abbaa")]
+        [DataRow("abbaa")]
+        [DataRow("aabbcc")]
+        [DataRow("aabbccb")]
+        public void IsPalindromePermutation_IsPermutation_ReturnsTrue(string input)
+        {
+            // Arrange
+
+            // Act
+            bool actual = Palindrome.IsPalindromePermutation(input);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        [DataRow("ab")]
+        [DataRow("abaa")]
+        [DataRow("abc")]
+        [DataRow("abcddcbb")]
+        [DataRow("aaabbccbc")]
+        [DataRow("aabbccbc")]
+        public void IsPalindromePermutation_IsNotPermutation_ReturnsFalse(string input)
+        {
+            // Arrange
+
+            // Act
+            bool actual = Palindrome.IsPalindromePermutation(input);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}
