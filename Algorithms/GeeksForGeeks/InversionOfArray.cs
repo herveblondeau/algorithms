508bbbbffd5c03198af04e094388645e494af771
Hervé Blondeau <herve@projet-nu.fr>
2020-06-22 09:06:24 +0200

    Add inversion of array

---
+++ Algorithms/GeeksForGeeks/InversionOfArray.cs
@@ -0,0 +1,85 @@
+﻿// https://practice.geeksforgeeks.org/problems/inversion-of-array/0/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace GeeksForGeeks.InversionOfArray
+{
+    public class InversionOfArray
+    {
+        // O(nlogn) time, O(n) space
+        public int GetInversionCount(int[] values)
+        {
+            int inversionCount = 0;
+            int position;
+            List<int> reverseOrderedValues = new List<int>();
+
+            // For each element, do a 0(logn) search to find where it would fit in a reverse ordered list composed of the previous items: this is equivalent to counting how many previous items are greater
+            foreach(int value in values)
+            {
+                position = GetReverseOrderPosition(reverseOrderedValues, value);
+                reverseOrderedValues.Insert(position, value);
+                inversionCount += position;
+            }
+            return inversionCount;
+        }
+
+        private int GetReverseOrderPosition(List<int> reverseOrderedValues, int value)
+        {
+            return GetReverseOrderPositionInRange(reverseOrderedValues, value, 0, Math.Max(reverseOrderedValues.Count - 1, 0));
+        }
+
+        // TODO: streamline this algorithm (which is essentially a binary search)
+        private int GetReverseOrderPositionInRange(List<int> reverseOrderedValues, int value, int start, int end)
+        {
+            if (reverseOrderedValues.Count == 0 && start == 0 && end == 0)
+            {
+                return 0;
+            }
+
+            if (start >= reverseOrderedValues.Count || end >= reverseOrderedValues.Count)
+            {
+                return -1;
+            }
+
+            if (start == end)
+            {
+                return value >= reverseOrderedValues[start] ? start : start + 1;
+            }
+
+            int position = start + (end - start) / 2;
+            if (value > reverseOrderedValues[position])
+            {
+                return GetReverseOrderPositionInRange(reverseOrderedValues, value, start, position);
+            }
+            else if (value < reverseOrderedValues[position])
+            {
+                return GetReverseOrderPositionInRange(reverseOrderedValues, value, position + 1, end);
+            }
+            else
+            {
+                return position;
+            }
+        }
+
+        // O(n2) time, O(1) space
+        public int GetInversionCount1(int[] elements)
+        {
+            int inversionCount = 0;
+
+            // Just count, for each element, the number of elements before it that are greater
+            for (int i = 0; i < elements.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (elements[i] < elements[j])
+                    {
+                        inversionCount++;
+                    }
+                }
+            }
+            return inversionCount;
+        }
+    }
+}
---
+++ AlgorithmsTests/GeeksForGeeks/InversionOfArrayTests.cs
@@ -0,0 +1,41 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeeksForGeeks.InversionOfArray
+{
+    [TestClass]
+    public class InversionOfArrayTests
+    {
+
+        [TestMethod]
+        [DataRow(new int[] { 1 })]
+        [DataRow(new int[] { 1, 2 })]
+        [DataRow(new int[] { 1, 2, 3, 4 })]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 })]
+        public void GetInversionCount_OrderedList_Returns0(int[] values)
+        {
+            // Arrange
+            InversionOfArray inversionOfArraySolver = new InversionOfArray();
+
+            // Act
+            var actual = inversionOfArraySolver.GetInversionCount(values);
+
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 4, 1 }, 1)]
+        [DataRow(new int[] { 2, 4, 1, 3, 5 }, 3)]
+        public void GetInversionCount_UnorderedList_PerformsCorrectly(int[] values, int expected)
+        {
+            // Arrange
+            InversionOfArray inversionOfArraySolver = new InversionOfArray();
+
+            // Act
+            var actual = inversionOfArraySolver.GetInversionCount(values);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
