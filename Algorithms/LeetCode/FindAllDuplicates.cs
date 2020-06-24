c54585fcf7b94ec10208e65eb77756ba1c13985b
Hervé Blondeau <herve@projet-nu.fr>
2020-06-24 07:38:45 +0200

    Add find all duplicates

---
+++ Algorithms/LeetCode/FindAllDuplicates.cs
@@ -0,0 +1,29 @@
+﻿// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+using System.Collections.Generic;
+
+namespace LeetCode.FindAllDuplicates
+{
+    public class ArrayDuplicates
+    {
+        // O(n) time, O(n) space
+        public static int[] FindAllDuplicates(int[] values)
+        {
+            HashSet<int> visitedValues = new HashSet<int>();
+            List<int> duplicates = new List<int>();
+
+            foreach (var value in values)
+            {
+                if (visitedValues.Contains(value))
+                {
+                    duplicates.Add(value);
+                }
+                else
+                {
+                    visitedValues.Add(value);
+                }
+
+            }
+            return duplicates.ToArray();
+        }
+    }
+}
---
+++ AlgorithmsTests/LeetCode/FindAllDuplicatesTests.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LeetCode.FindAllDuplicates
+{
+    [TestClass]
+    public class ArrayDuplicatesTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { })]
+        [DataRow(new int[] { 1 })]
+        [DataRow(new int[] { 1, 2 })]
+        [DataRow(new int[] { 1, 2, 10, 53, 546 })]
+        public void FindAllDuplicates_NoDuplicate_ReturnsEmptyArray(int[] values)
+        {
+            // Arrange
+
+            // Act
+            int[] actual = ArrayDuplicates.FindAllDuplicates(values);
+
+            // Assert
+            Assert.AreEqual(0, actual.Length);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 1 }, new int[] { 1 })]
+        [DataRow(new int[] { 1, 2, 1 }, new int[] { 1 })]
+        [DataRow(new int[] { 4, 3, 2, 7, 8, 2, 3, 1 }, new int[] { 2, 3 })]
+        public void FindAllDuplicates_Duplicates_ReturnsDuplicates(int[] values, int[] expected)
+        {
+            // Arrange
+
+            // Act
+            int[] actual = ArrayDuplicates.FindAllDuplicates(values);
+
+            // Assert
+            Array.Sort(expected);
+            Array.Sort(actual);
+            Assert.AreEqual(expected.Length, actual.Length);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
