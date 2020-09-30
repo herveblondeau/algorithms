d3bdbbe6647c543a7874267e8e7631a5ce26c0d7
Hervé Blondeau <herve.blondeau@gmail.com>
2020-09-30 10:29:53 +0200

    Implement correct solution for String rotation

--- Algorithms/CrackingTheCodeInterview/ArraysAndStrings/StringRotation.cs
+++ Algorithms/CrackingTheCodeInterview/ArraysAndStrings/StringRotation.cs
@@ -15,11 +15,16 @@ namespace CrackingTheCodeInterview.ArraysAndStrings.StringRotation
                 return false;
             }
 
-            char str1FirstChar = str1[0];
+            return AreRotationsMethod2(str1, str2);
+        }
 
-            // We loop through each character of the second string. Every time we find the first string's beginning character, we rotate the second string and compare it to the first one
-            // For example, if the first string is "waterbottlewow" and the second one is "erbottlewowwater", we will compare "wowwaterbottle", "wwaterbottlewo" and "waterbottlewow" to the first string
-            // Note: this solution works but it doesn't follow the actual exercise requirement, which is to use one call to IsSubstring()
+        // We loop through each character of the second string. Every time we find the first string's beginning character, we rotate the second string and compare it to the first one
+        // For example, if the first string is "waterbottlewow" and the second one is "erbottlewowwater", we will compare "wowwaterbottle", "wwaterbottlewo" and "waterbottlewow" to the first string
+        // Note: this solution works but it doesn't follow the actual exercise requirement, which is to use one call to IsSubstring(), so method 2 is recommended instead
+        private static bool AreRotationsMethod1(string str1, string str2)
+        {
+
+            char str1FirstChar = str1[0];
             for (int i = 0; i < str2.Length; i++)
             {
                 if (str2[i] == str1FirstChar)
@@ -34,10 +39,17 @@ namespace CrackingTheCodeInterview.ArraysAndStrings.StringRotation
             return false;
         }
 
+        // Using the given "waterbottle" example, all rotations are contained within the "waterbottlewaterbottle" string
+        // This solution fits the exercise requirement
+        private static bool AreRotationsMethod2(string str1, string str2)
+        {
+            return IsSubstring(str2, str1 + str1);
+        }
+
         // Checks whether str1 is a substring of str2
         private static bool IsSubstring(string str1, string str2)
         {
-            return str2.Contains(str2);
+            return str2.Contains(str1);
         }
     }
 }
