e84b818582c4baa83ef9d2d8e5f9e98d9f6a1801
Hervé Blondeau <herve.blondeau@gmail.com>
2020-10-05 08:34:56 +0200

    Add Queue via stacks

---
+++ Algorithms/CrackingTheCodeInterview/StacksAndQueues/QueueViaStacks.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrackingTheCodeInterview.StacksAndQueues.QueueViaStacks
+{
+    // Source: "Cracking the coding interview" book (3.4 - Queue via stacks)
+    public class QueueViaStacks
+    {
+        // Simulating a queue with a stack would imply adding elements at the bottom of the stack, which is impossible
+        // However, using a temporary stack, we can make sure the elements are always kept in the FIFO order
+        // When we want to enqueue a value, we:
+        // - pop all elements to a temporary stack, reversing their order
+        // - add the value to the actual stack, making it the bottom element
+        // - pop the temporary stack elements back to the actual stack
+        private Stack<int> _values;
+        private Stack<int> _temp;
+
+        public QueueViaStacks()
+        {
+            _values = new Stack<int>();
+            _temp = new Stack<int>();
+        }
+
+        // O(n) time
+        public void Enqueue(int value)
+        {
+            // Pop all elements from the actual stack and push them to the temporary stack
+            // This reverses their order
+            while (_values.Count > 0)
+            {
+                _temp.Push(_values.Pop());
+            }
+
+            // Push the new value to the actual stack
+            // The value therefore goes at the bottom
+            _values.Push(value); ;
+
+            // Pop all elements from the actual stack and push them to the temporary stack
+            // This places them in their initial order, on top of the new value
+            while (_temp.Count > 0)
+            {
+                _values.Push(_temp.Pop());
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (_values.Count > 0)
+            {
+                return _values.Pop();
+            }
+
+            return Int32.MinValue;
+        }
+
+        public int Peek()
+        {
+            if (_values.Count > 0)
+            {
+                return _values.Peek();
+            }
+
+            return Int32.MinValue;
+        }
+
+    }
+}
---
+++ AlgorithmsTests/CrackingTheCodeInterview/StacksAndQueues/QueueViaStacksTests.cs
@@ -0,0 +1,58 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CrackingTheCodeInterview.StacksAndQueues.QueueViaStacks
+{
+    [TestClass]
+    public class QueueViaStacksTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3 }, 1)]
+        [DataRow(new int[] { 3, 2, 1 }, 3)]
+        [DataRow(new int[] { 2, 1, 3 }, 2)]
+        public void Dequeue_EnqueueThenPeek_PerformsCorrectly(int[] inputs, int expected)
+        {
+            // Arrange
+            QueueViaStacks queueViaStacks = new QueueViaStacks();
+            foreach (int n in inputs)
+            {
+                queueViaStacks.Enqueue(n);
+            }
+
+            // Act
+            int actual = queueViaStacks.Peek();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, 1, 1)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, 2, 2)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, 3, 3)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, 4, 4)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, 5, 5)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, 6, 6)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6 }, 7, Int32.MinValue)]
+        public void Dequeue_EnqueueThenDequeueSeveralTimes_PerformsCorrectly(int[] inputs, int nbDequeues, int expected)
+        {
+            // Arrange
+            QueueViaStacks queueViaStacks = new QueueViaStacks();
+            foreach (int n in inputs)
+            {
+                queueViaStacks.Enqueue(n);
+            }
+
+            // Act
+            int actual = Int32.MinValue;
+            for (int i = 0; i < nbDequeues; i++)
+            {
+                actual = queueViaStacks.Dequeue();
+            }
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
