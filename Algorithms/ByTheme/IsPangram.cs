8c3c85bba41d0f622e5ed136d0712e98ece84d03
Hervé Blondeau <herve.blondeau@gmail.com>
2022-03-20 09:47:57 +0100

    Add is pangram

---
+++ Algorithms/ByTheme/IsPangram.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ByTheme.IsPangram
+{
+    public class IsPangram
+    {
+        public bool Check(string text)
+        {
+            // Complexity: O(n) time, O(1) space
+            HashSet<char> alphabet = new HashSet<char>() { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            foreach (var c in text.ToUpper())
+            {
+                alphabet.Remove(c);
+            }
+
+            return alphabet.Count == 0;
+        }
+    }
+}
---
+++ AlgorithmsTests/ByTheme/IsPangramTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ByTheme.IsPangram
+{
+    [TestClass]
+    public class IsPangramTests
+    {
+
+        [TestMethod]
+        [DataRow("alkgfkljgflkj", false)]
+        [DataRow("abcdefghijklmnopqrstuvwxy", false)]
+        [DataRow("abcdefghijklmnopqrstuvwxyz", true)]
+        [DataRow("abcdefGhijklmnoPQRstuVWxyz", true)]
+        [DataRow("abcde fghijk lmno pqrs   tuvw xyz", true)]
+        [DataRow("abc54908de fghijk lm43no pq%./?§rs   tuvw xyz", true)]
+        [DataRow("bcdenopfg hijaklmqruvw stxyz", true)]
+        [DataRow("The quick brown fox jumps over the lazy dog", true)]
+        public void Check_WhenCalled_PerformsCorrectly(string text, bool expected)
+        {
+            // Arrange
+            IsPangram isPangram = new IsPangram();
+
+            // Act
+            var actual = isPangram.Check(text);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
