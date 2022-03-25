6ace34207388dfd442166a585d4b246d365bd0ce
Hervé Blondeau <herve.blondeau@gmail.com>
2022-03-25 10:56:53 +0100

    Add Codingame's Closest number - not completed yet

---
+++ Algorithms/Codingame/ClosestNumber.cs
@@ -0,0 +1,135 @@
+﻿// https://www.codingame.com/training/hard/closest-number
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codingame.ClosestNumber
+{
+    public class ClosestNumber
+    {
+        public string GetClosestPermutation(string target, string source)
+        {
+            // Build a dictionary of the digits available for permutation
+            Dictionary<int, int> availableDigits = new Dictionary<int, int>();
+            for (int i = 0; i < source.Length; i++)
+            {
+                int d = _parse(source[i]);
+
+                if (availableDigits.ContainsKey(d))
+                {
+                    availableDigits[d]++;
+                }
+                else
+                {
+                    availableDigits.Add(d, 1);
+                }
+            }
+
+            StringBuilder permutation = new StringBuilder();
+            int index = 0;
+            int digit = -1;
+
+            // As long as we can find the same digits as the target, just add them
+            while (index < target.Length)
+            {
+                digit = _parse(target[index]);
+
+                if (!availableDigits.ContainsKey(digit))
+                {
+                    break;
+                }
+
+                permutation.Append(digit);
+                _removeDigit(digit, availableDigits);
+                index++;
+            }
+
+            // Then find the closest next digit
+            bool? searchHigher = null;
+            if (index < target.Length - 1)
+            {
+                digit = (FindClosestDigit(_parse(target[index]), _parse(target[index + 1]), availableDigits));
+                permutation.Append(digit);
+                _removeDigit(digit, availableDigits);
+                searchHigher = digit < _parse(target[index]);
+            }
+
+            // Then find the highest or lowest digits
+            if (searchHigher == true)
+            {
+                digit = 9;
+                while (digit >= 0)
+                {
+                    while (availableDigits.ContainsKey(digit))
+                    {
+                        permutation.Append(digit);
+                        _removeDigit(digit, availableDigits);
+                    }
+                    digit--;
+                }
+            }
+            else if (searchHigher == false)
+            {
+                digit = 0;
+                while (digit <= 9)
+                {
+                    while (availableDigits.ContainsKey(digit))
+                    {
+                        permutation.Append(digit);
+                        _removeDigit(digit, availableDigits);
+                    }
+                    digit++;
+                }
+            }
+
+            return permutation.ToString();
+        }
+
+        private int FindClosestDigit(int current, int next, Dictionary<int, int> availableDigits)
+        {
+            int digit = current;
+            int increment = next >= 5 ? 1 : -1;
+
+            while (digit >= 0 || digit <= 9)
+            {
+                if (availableDigits.ContainsKey(digit))
+                {
+                    return digit;
+                }
+
+                digit += increment;
+                increment = increment > 0 ? -increment-1 : -increment+1;
+
+                if (digit < 0 || digit > 9)
+                {
+                    digit += increment;
+                    increment = increment > 0 ? -increment - 1 : -increment + 1;
+                }
+            }
+
+            return 0;
+        }
+
+        private void _removeDigit(int digit, Dictionary<int, int> availableDigits)
+        {
+            if (availableDigits.ContainsKey(digit))
+            {
+                if (availableDigits[digit] > 1)
+                {
+                    availableDigits[digit]--;
+                }
+                else
+                {
+                    availableDigits.Remove(digit);
+                }
+            }
+        }
+
+        private int _parse(char c)
+        {
+            return c - '0';
+        }
+    }
+}
---
+++ AlgorithmsTests/Codingame/ClosestNumberTests.cs
@@ -0,0 +1,62 @@
+using Codingame.Staircases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Codingame.ClosestNumber
+{
+    [TestClass]
+    public class ClosestNumberTests
+    {
+
+        [TestMethod]
+        [DataRow("94754")]
+        [DataRow("5")]
+        [DataRow("7816053167431")]
+        public void GetClosestPermutation_SameSourceAndTarget_ReturnsTarget(string target)
+        {
+            // Arrange
+            ClosestNumber closestNumber = new ClosestNumber();
+
+            // Act
+            var actual = closestNumber.GetClosestPermutation(target, target);
+
+            // Assert
+            Assert.AreEqual(target, actual);
+        }
+
+        [TestMethod]
+        //[DataRow("94754", "38413", "84331")]
+        [DataRow("9", "7", "7")]
+        //[DataRow("1234", "5678", "5678")]
+        //[DataRow("5743211", "1111114", "4111111")]
+        //[DataRow("5743", "1111", "1111")]
+        //[DataRow("94545", "87645", "87654")]
+        //[DataRow("64545", "57645", "64557")]
+        public void GetClosestPermutation_SameLengthNoZero_PerformsCorrectly(string target, string source, string expected)
+        {
+            // Arrange
+            ClosestNumber closestNumber = new ClosestNumber();
+
+            // Act
+            var actual = closestNumber.GetClosestPermutation(target, source);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow("94754", "3841", "8431")]
+        public void GetMinimumDrops_WhenCalled_PerformsCorrectly(string target, string source, string expected)
+        {
+            // Arrange
+            ClosestNumber closestNumber = new ClosestNumber();
+
+            // Act
+            var actual = closestNumber.GetClosestPermutation(target, source);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
