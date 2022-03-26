5ccc35404c969fc921cb8a1ce8b175ff5be71a65
Hervé Blondeau <herve.blondeau@gmail.com>
2022-03-26 19:20:47 +0100

    Add Codingame's Staircases

---
+++ AlgorithmsTests/Codingame/StaircasesTests.cs
@@ -0,0 +1,46 @@
+using Codingame.Staircases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Codingame.Staircases
+{
+    [TestClass]
+    public class StaircasesTests
+    {
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
+        public void GetNumberOfStaircases_NotEnoughBricks_ReturnsZero(int bricks)
+        {
+            // Arrange
+            Staircases staircases = new Staircases();
+
+            // Act
+            var actual = staircases.GetNumberOfStaircases(bricks);
+
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        [DataRow(3, 1)]
+        [DataRow(5, 2)]
+        [DataRow(10, 9)]
+        [DataRow(25, 141)]
+        //[DataRow(500, 2300)]
+        public void GetNumberOfStaircases_WhenCalled_PerformsCorrectly(int bricks, int expected)
+        {
+            // Arrange
+            Staircases staircases = new Staircases();
+
+            // Act
+            var actual = staircases.GetNumberOfStaircases(bricks);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
