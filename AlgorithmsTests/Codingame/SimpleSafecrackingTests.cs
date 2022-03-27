bac8918478211266c9a263b91e7942a9e2bb7c77
Hervé Blondeau <herve.blondeau@gmail.com>
2022-03-27 18:31:33 +0200

    Add Codingame's Simple safecracking

---
+++ AlgorithmsTests/Codingame/SimpleSafecrackingTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codingame.SimpleSafecracking
+{
+    [TestClass]
+    public class SimpleSafecrackingTests
+    {
+        [TestMethod]
+        [DataRow("Aol zhml jvtipuhapvu pz: zpe-mvby-zpe-mvby-aoyll", 64643)]
+        [DataRow("Wkh vdih frpelqdwlrq lv: wkuhh-ilyh-rqh-vla-wzr", 35162)]
+        [DataRow("Dro ckpo mywlsxkdsyx sc: pyeb-xsxo-yxo-osqrd-dgy-joby-dgy-yxo-csh-drboo", 4918202163)]
+        [DataRow("Ymj xfkj htrgnsfynts nx: ktzw-ejwt-ejwt-ktzw-ktzw-ejwt-jnlmy-xjajs-xjajs", 400440877)]
+        public void GetNthLine_WhenCalled_PerformsCorrectly(string encoded, long expected)
+        {
+            // Arrange
+            SimpleSafecracking simpleSafecracking = new SimpleSafecracking();
+
+            // Act
+            var actual = simpleSafecracking.Decode(encoded);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
