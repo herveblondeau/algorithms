f49471cfc8f40e28c66260f642f214f71a385598
Hervé Blondeau <herve.blondeau@gmail.com>
2022-04-04 17:26:21 +0200

    Add Codingame's Unfolding paper

---
+++ AlgorithmsTests/Codingame/UnfoldingPaperTests.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Codingame.UnfoldingPaper
+{
+    [TestClass]
+    public class UnfoldingPaperTests
+    {
+
+        [TestMethod]
+        [DataRow(1, 5)]
+        [DataRow(2, 10)]
+        [DataRow(5, 290)]
+        public void Unfold_3x3FoldedPaper_PerformsCorrectly(int nbUnfoldings, int expected)
+        {
+            // Arrange
+            UnfoldingPaper unfoldingPaper = new UnfoldingPaper();
+            string[] folded = new string[]
+            {
+                "###",
+                "#..",
+                "#.#"
+            };
+
+            // Act
+            var actual = unfoldingPaper.Unfold(folded, nbUnfoldings);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(1)]
+        [DataRow(5)]
+        [DataRow(14)]
+        [DataRow(10000)]
+        public void Unfold_Full_AlwaysOne(int nbUnfoldings)
+        {
+            // Arrange
+            UnfoldingPaper unfoldingPaper = new UnfoldingPaper();
+            string[] folded = new string[]
+            {
+                "###",
+                "#.#",
+                "###"
+            };
+
+            // Act
+            var actual = unfoldingPaper.Unfold(folded, nbUnfoldings);
+
+            // Assert
+            Assert.AreEqual(1, actual);
+        }
+    }
+}
