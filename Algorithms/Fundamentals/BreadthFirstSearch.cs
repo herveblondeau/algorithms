0549cda7a23c1f40391e38ac3249a9bd5e196272
Hervé BLONDEAU <hblondeau-ext@alpheys.com>
2024-06-12 18:51:20 +0200

    Simplify breadth first search structure

--- Algorithms/Fundamentals/BreadthFirstSearch.cs
+++ Algorithms/Fundamentals/BreadthFirstSearch.cs
@@ -4,24 +4,34 @@ using System.Linq;
 
 namespace Fundamentals.BreadthFirstSearch;
 
-public class BreadthFirstSearch
+public class BreadthFirstSearch<T> where T : IEquatable<T>
 {
-    public bool HasPath<T>(Node<T> from, Node<T> to) where T : IEquatable<T>
+    public Dictionary<T, List<T>> Graph = null!;
+
+    public BreadthFirstSearch(Dictionary<T, List<T>> graph)
     {
-        HashSet<Node<T>> visited = new();
-        Queue<Node<T>> queue = new();
+        Graph = graph;
+    }
+
+    public bool HasPath(T from, T to)
+    {
+        HashSet<T> visited = new();
+        Queue<T> queue = new();
 
         var node = from;
         while (node is not null)
         {
-            if (node == to)
+            if (node.Equals(to))
             {
                 return true;
             }
 
-            foreach (var neighbor in node.Destinations.Where(n => !visited.Contains(n)))
+            if (Graph.ContainsKey(node))
             {
-                queue.Enqueue(neighbor);
+                foreach (var neighbor in Graph[node].Where(n => !visited.Contains(n)))
+                {
+                    queue.Enqueue(neighbor);
+                }
             }
 
             visited.Add(node);
@@ -30,38 +40,9 @@ public class BreadthFirstSearch
 
         return false;
     }
-}
 
-public class Node<T> : IEquatable<Node<T>> where T : IEquatable<T>
-{
-    public T Value { get; set; }
-    public List<Node<T>> Destinations { get; set; } = null!;
-
-    public Node(T value)
+    public List<T> GetShortestPath(T from, T to)
     {
-        Value = value;
-        Destinations = new();
-    }
-
-    public Node<T> AddNeighbors(List<Node<T>> neighbors)
-    {
-        Destinations.AddRange(neighbors);
-        return this;
-    }
-
-    public Node<T> AddNeighbor(Node<T> neighbor)
-    {
-        Destinations.Add(neighbor);
-        return this;
-    }
-
-    public bool Equals(Node<T>? other)
-    {
-        if (other is null)
-        {
-            return false;
-        }
-
-        return Value.Equals(other.Value);
+        return new();
     }
 }
--- AlgorithmsTests/Fundamentals/BreadthFirstSearchTests.cs
+++ AlgorithmsTests/Fundamentals/BreadthFirstSearchTests.cs
@@ -6,150 +6,98 @@ namespace Fundamentals.BreadthFirstSearch
     [TestClass]
     public class BreadthFirstSearchTests
     {
-        private static Node<string> NodeA = null!;
-        private static Node<string> NodeB = null!;
-        private static Node<string> NodeC = null!;
-        private static Node<string> NodeD = null!;
-        private static Node<string> NodeE = null!;
-        private static Node<string> NodeF = null!;
-        private static Node<string> NodeG = null!;
-        private static Node<string> NodeH = null!;
-        private static Node<string> NodeI = null!;
-
-        [ClassInitialize]
-        public static void GenerateNodes(TestContext context)
-        {
-            NodeA = new("A");
-            NodeB = new("B");
-            NodeC = new("C");
-            NodeD = new("D");
-            NodeE = new("E");
-            NodeF = new("F");
-            NodeG = new("G");
-            NodeH = new("H");
-            NodeI = new("I");
-            NodeA.AddNeighbors([NodeB, NodeC, NodeD]);
-            NodeB.AddNeighbor(NodeE);
-            NodeC.AddNeighbors([NodeE, NodeG]);
-            NodeE.AddNeighbor(NodeF);
-            NodeH.AddNeighbor(NodeI);
-            NodeI.AddNeighbor(NodeH);
-        }
-
         [TestMethod]
-        [DynamicData(nameof(HasPath_WhenConnected_ReturnsTrue_Data), DynamicDataSourceType.Method)]
-        public void HasPath_WhenConnected_ReturnsTrue(Node<string> from, Node<string> to)
+        [DataRow("A", "B", true)]
+        [DataRow("A", "C", true)]
+        [DataRow("A", "D", true)]
+        [DataRow("A", "E", true)]
+        [DataRow("A", "F", true)]
+        [DataRow("A", "G", true)]
+        [DataRow("B", "E", true)]
+        [DataRow("B", "F", true)]
+        [DataRow("C", "G", true)]
+        [DataRow("H", "I", true)]
+        [DataRow("I", "H", true)]
+        [DataRow("B", "A", false)]
+        [DataRow("C", "A", false)]
+        [DataRow("D", "A", false)]
+        [DataRow("E", "A", false)]
+        [DataRow("F", "A", false)]
+        [DataRow("G", "A", false)]
+        [DataRow("H", "A", false)]
+        [DataRow("I", "A", false)]
+        [DataRow("C", "B", false)]
+        [DataRow("D", "B", false)]
+        [DataRow("E", "B", false)]
+        [DataRow("F", "B", false)]
+        [DataRow("G", "B", false)]
+        [DataRow("H", "B", false)]
+        [DataRow("I", "B", false)]
+        [DataRow("B", "C", false)]
+        [DataRow("D", "C", false)]
+        [DataRow("E", "C", false)]
+        [DataRow("F", "C", false)]
+        [DataRow("G", "C", false)]
+        [DataRow("H", "C", false)]
+        [DataRow("I", "C", false)]
+        [DataRow("B", "D", false)]
+        [DataRow("C", "D", false)]
+        [DataRow("E", "D", false)]
+        [DataRow("F", "D", false)]
+        [DataRow("G", "D", false)]
+        [DataRow("H", "D", false)]
+        [DataRow("I", "D", false)]
+        [DataRow("D", "E", false)]
+        [DataRow("F", "E", false)]
+        [DataRow("G", "E", false)]
+        [DataRow("H", "E", false)]
+        [DataRow("I", "E", false)]
+        [DataRow("D", "F", false)]
+        [DataRow("G", "F", false)]
+        [DataRow("H", "F", false)]
+        [DataRow("I", "F", false)]
+        [DataRow("B", "G", false)]
+        [DataRow("D", "G", false)]
+        [DataRow("E", "G", false)]
+        [DataRow("F", "G", false)]
+        [DataRow("H", "G", false)]
+        [DataRow("I", "G", false)]
+        [DataRow("A", "H", false)]
+        [DataRow("B", "H", false)]
+        [DataRow("C", "H", false)]
+        [DataRow("D", "H", false)]
+        [DataRow("E", "H", false)]
+        [DataRow("F", "H", false)]
+        [DataRow("G", "H", false)]
+        [DataRow("A", "I", false)]
+        [DataRow("B", "I", false)]
+        [DataRow("C", "I", false)]
+        [DataRow("D", "I", false)]
+        [DataRow("E", "I", false)]
+        [DataRow("F", "I", false)]
+        [DataRow("G", "I", false)]
+        public void HasPath_WhenCalled_PerformsCorrectly(string from, string to, bool expected)
         {
             // Arrange
-            BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
+            Dictionary<string, List<string>> graph = new()
+            {
+                { "A", ["B", "C", "D"] },
+                { "B", ["E", "F"] },
+                { "C", ["G"] },
+                { "D", [] },
+                { "E", [] },
+                { "F", [] },
+                { "G", [] },
+                { "H", ["I"] },
+                { "I", ["H"] },
+            };
+            BreadthFirstSearch<string> breadthFirstSearch = new(graph);
 
             // Act
             var actual = breadthFirstSearch.HasPath(from, to);
 
             // Assert
-            Assert.IsTrue(actual);
-        }
-
-        public static IEnumerable<object[]> HasPath_WhenConnected_ReturnsTrue_Data()
-        {
-            yield return new object[] { NodeA, NodeB };
-            yield return new object[] { NodeA, NodeC };
-            yield return new object[] { NodeA, NodeD };
-            yield return new object[] { NodeA, NodeE };
-            yield return new object[] { NodeA, NodeF };
-            yield return new object[] { NodeA, NodeG };
-
-            yield return new object[] { NodeB, NodeE };
-            yield return new object[] { NodeB, NodeF };
-            yield return new object[] { NodeC, NodeG };
-
-            yield return new object[] { NodeH, NodeI };
-
-            yield return new object[] { NodeI, NodeH };
-        }
-
-        [TestMethod]
-        [DynamicData(nameof(HasPath_WhenNotConnected_ReturnsFalse_Data), DynamicDataSourceType.Method)]
-        public void HasPath_WhenNotConnected_ReturnsFalse(Node<string> from, Node<string> to)
-        {
-            // Arrange
-            BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
-
-            // Act
-            var actual = breadthFirstSearch.HasPath(from, to);
-
-            // Assert
-            Assert.IsFalse(actual);
-        }
-
-        public static IEnumerable<object[]> HasPath_WhenNotConnected_ReturnsFalse_Data()
-        {
-            yield return new object[] { NodeB, NodeA };
-            yield return new object[] { NodeC, NodeA };
-            yield return new object[] { NodeD, NodeA };
-            yield return new object[] { NodeE, NodeA };
-            yield return new object[] { NodeF, NodeA };
-            yield return new object[] { NodeG, NodeA };
-            yield return new object[] { NodeH, NodeA };
-            yield return new object[] { NodeI, NodeA };
-
-            yield return new object[] { NodeC, NodeB };
-            yield return new object[] { NodeD, NodeB };
-            yield return new object[] { NodeE, NodeB };
-            yield return new object[] { NodeF, NodeB };
-            yield return new object[] { NodeG, NodeB };
-            yield return new object[] { NodeH, NodeB };
-            yield return new object[] { NodeI, NodeB };
-
-            yield return new object[] { NodeB, NodeC };
-            yield return new object[] { NodeD, NodeC };
-            yield return new object[] { NodeE, NodeC };
-            yield return new object[] { NodeF, NodeC };
-            yield return new object[] { NodeG, NodeC };
-            yield return new object[] { NodeH, NodeC };
-            yield return new object[] { NodeI, NodeC };
-
-            yield return new object[] { NodeB, NodeD };
-            yield return new object[] { NodeC, NodeD };
-            yield return new object[] { NodeE, NodeD };
-            yield return new object[] { NodeF, NodeD };
-            yield return new object[] { NodeG, NodeD };
-            yield return new object[] { NodeH, NodeD };
-            yield return new object[] { NodeI, NodeD };
-
-            yield return new object[] { NodeD, NodeE };
-            yield return new object[] { NodeF, NodeE };
-            yield return new object[] { NodeG, NodeE };
-            yield return new object[] { NodeH, NodeE };
-            yield return new object[] { NodeI, NodeE };
-
-            yield return new object[] { NodeD, NodeF };
-            yield return new object[] { NodeG, NodeF };
-            yield return new object[] { NodeH, NodeF };
-            yield return new object[] { NodeI, NodeF };
-
-            yield return new object[] { NodeB, NodeG };
-            yield return new object[] { NodeD, NodeG };
-            yield return new object[] { NodeE, NodeG };
-            yield return new object[] { NodeF, NodeG };
-            yield return new object[] { NodeH, NodeG };
-            yield return new object[] { NodeI, NodeG };
-
-            yield return new object[] { NodeA, NodeH };
-            yield return new object[] { NodeB, NodeH };
-            yield return new object[] { NodeC, NodeH };
-            yield return new object[] { NodeD, NodeH };
-            yield return new object[] { NodeE, NodeH };
-            yield return new object[] { NodeF, NodeH };
-            yield return new object[] { NodeG, NodeH };
-
-            yield return new object[] { NodeA, NodeI };
-            yield return new object[] { NodeB, NodeI };
-            yield return new object[] { NodeC, NodeI };
-            yield return new object[] { NodeD, NodeI };
-            yield return new object[] { NodeE, NodeI };
-            yield return new object[] { NodeF, NodeI };
-            yield return new object[] { NodeG, NodeI };
+            Assert.AreEqual(expected, actual);
         }
     }
 }
