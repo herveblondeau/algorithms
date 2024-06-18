30977fcab497ecf6d48309092e28024f4c7a1d0e
Hervé BLONDEAU <hblondeau-ext@alpheys.com>
2024-06-18 07:16:41 +0200

    Add The Barnyard

---
+++ Algorithms/Codingame/TheBarnyard.cs
@@ -0,0 +1,134 @@
+﻿// https://www.codingame.com/training/expert/the-barnyard
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codingame.TheBarnyard;
+
+public class Barnyard
+{
+    private static readonly Dictionary<(string, string), int> _partCountsPerAnimal = new Dictionary<(string, string), int>
+    {
+        { ("Rabbits", "Heads"),  1 },
+        { ("Rabbits", "Eyes"),   2 },
+        { ("Rabbits", "Horns"),  0 },
+        { ("Rabbits", "Wings"),  0 },
+        { ("Rabbits", "Legs"),   4 },
+        { ("Chickens", "Heads"), 1 },
+        { ("Chickens", "Eyes"),  2 },
+        { ("Chickens", "Horns"), 0 },
+        { ("Chickens", "Wings"), 2 },
+        { ("Chickens", "Legs"),  2 },
+        { ("Cows", "Heads"),     1 },
+        { ("Cows", "Eyes"),      2 },
+        { ("Cows", "Horns"),     2 },
+        { ("Cows", "Wings"),     0 },
+        { ("Cows", "Legs"),      4 },
+        { ("Pegasi", "Heads"),   1 },
+        { ("Pegasi", "Eyes"),    2 },
+        { ("Pegasi", "Horns"),   0 },
+        { ("Pegasi", "Wings"),   2 },
+        { ("Pegasi", "Legs"),    4 },
+        { ("Demons", "Heads"),   1 },
+        { ("Demons", "Eyes"),    4 },
+        { ("Demons", "Horns"),   4 },
+        { ("Demons", "Wings"),   2 },
+        { ("Demons", "Legs"),    4 },
+    };
+
+    public Dictionary<string, int> GetAnimalCounts(string[] animals, Dictionary<string, int> bodyParts)
+    {
+        // This is basically a n-equation system
+        // It is greatly simplified because we know that there are no more than 5 variables, and that the system always has exactly one solution
+
+        int[][] equations = new int[animals.Length][];
+        int index = 0;
+        foreach (var bodyPart in bodyParts)
+        {
+            equations[index] = new int[animals.Length + 1];
+            for (int i = 0; i < animals.Length; i++)
+            {
+                equations[index][i] = _partCountsPerAnimal[(animals[i], bodyPart.Key)];
+            }
+            equations[index][animals.Length] = bodyPart.Value;
+
+            index++;
+        }
+
+        int[] results = _solve(equations);
+
+        var nbAnimals = new Dictionary<string, int>();
+        for (int i = 0; i < animals.Length; i++)
+        {
+            nbAnimals.Add(animals[i], results[i]);
+        }
+
+        return nbAnimals;
+    }
+
+    private int[] _solve(int[][] equations)
+    {
+        int[] results = new int[equations.Length];
+
+        // Exit condition
+        if (equations.Length == 1)
+        {
+            return new int[] { equations[0][1] / equations[0][0] };
+        }
+
+        // Recursion
+        // - build subsystem
+        int[][] subEquations = new int[equations.Length - 1][];
+        int index = 0;
+        int index1 = 0;
+        int index2 = equations.Length - 1;
+        while (index1 < index2)
+        {
+            if (equations[index1][0] == 0)
+            {
+                subEquations[index] = equations[index1]
+                    .Skip(1)
+                    .ToArray();
+                index1++;
+            }
+            else if (equations[index2][0] == 0)
+            {
+                subEquations[index] = equations[index2]
+                    .Skip(1)
+                    .ToArray();
+                index2--;
+            }
+            else
+            {
+                subEquations[index] = equations[index1]
+                    .Zip(equations[index2], (x, y) => x * equations[index2][0] - y * equations[index1][0])
+                    .Skip(1)
+                    .ToArray();
+
+                index1++;
+            }
+
+            index++;
+        }
+
+        // - solve subsystem
+        int[] subSolutions = _solve(subEquations);
+
+        // - use first equation to solve first variable
+        var firstEquation = equations.Where(e => e[0] != 0).First();
+        var firstVariable = firstEquation[firstEquation.Length - 1];
+        for (int i = firstEquation.Length - 2; i > 0; i--)
+        {
+            firstVariable -= firstEquation[i] * subSolutions[i - 1];
+        }
+        firstVariable /= firstEquation[0];
+
+        results[0] = firstVariable;
+        for (int i = 1; i <= results.Length - 1; i++)
+        {
+            results[i] = subSolutions[i - 1];
+        }
+
+        return results;
+    }
+}
---
+++ AlgorithmsTests/Codingame/TheBarnyardTests.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codingame.TheBarnyard;
+
+[TestClass]
+public class TheBarnyardTests
+{
+
+    [TestMethod]
+    [DynamicData(nameof(GetAnimalCounts_WhenCalled_PerformsCorrectly_Data), DynamicDataSourceType.Method)]
+    public void GetAnimalCounts_WhenCalled_PerformsCorrectly(string[] animals, Dictionary<string, int> bodyParts, Dictionary<string, int> expected)
+    {
+        // Arrange
+        Barnyard Barnyard = new Barnyard();
+
+        // Act
+        var actual = Barnyard.GetAnimalCounts(animals, bodyParts);
+
+        // Assert
+        CollectionAssert.AreEqual(expected, actual);
+    }
+
+    private static IEnumerable<object[]> GetAnimalCounts_WhenCalled_PerformsCorrectly_Data()
+    {
+        yield return new object[] {
+            new string[]
+            {
+                "Rabbits",
+                "Chickens",
+            },
+            new Dictionary<string, int>
+            {
+                { "Heads", 5 },
+                { "Legs", 14 },
+            },
+            new Dictionary<string, int>
+            {
+                { "Rabbits", 2 },
+                { "Chickens", 3 },
+            }
+        };
+
+        yield return new object[] {
+            new string[]
+            {
+                "Rabbits",
+                "Chickens",
+                "Cows",
+            },
+            new Dictionary<string, int>
+            {
+                { "Heads", 9 },
+                { "Horns", 6 },
+                { "Wings", 8 },
+            },
+            new Dictionary<string, int>
+            {
+                { "Rabbits", 2 },
+                { "Chickens", 4 },
+                { "Cows", 3 },
+            }
+        };
+
+        yield return new object[] {
+            new string[]
+            {
+                "Cows",
+                "Pegasi",
+                "Demons",
+                "Chickens",
+                "Rabbits",
+            },
+            new Dictionary<string, int>
+            {
+                { "Eyes", 128 },
+                { "Heads", 61 },
+                { "Legs", 202 },
+                { "Wings", 72 },
+                { "Horns", 34 },
+            },
+            new Dictionary<string, int>
+            {
+                { "Cows", 11 },
+                { "Pegasi", 12 },
+                { "Demons", 3 },
+                { "Chickens", 21 },
+                { "Rabbits", 14 },
+            }
+        };
+    }
+}
