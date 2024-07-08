342b7ad1a2f56cd346dd99a7a9c06cc3ea094ec6
Hervé BLONDEAU <hblondeau-ext@alpheys.com>
2024-07-08 18:41:39 +0200

    Move files

--- AlgorithmsTests/Codingame/Hard/ClosestNumberTests.cs
+++ AlgorithmsTests/Codingame/Hard/ClosestNumberTests.cs
@@ -57,4 +57,57 @@ public class ClosestNumberTests
         actual.Should().Be(expected);
     }
 
+    [TestMethod]
+    public void GetMinimumDrops_CodingameLongestCommonPrefixIsNotAlwaysRight_Returns605()
+    {
+        // Arrange
+        ClosestNumber closestNumber = new();
+
+        // Act
+        var actual = closestNumber.GetClosestPermutation("590", "506");
+
+        // Assert
+        actual.Should().Be("605");
+    }
+
+    [TestMethod]
+    public void GetMinimumDrops_CodingameTwoOptimalPermutations_Returns95()
+    {
+        // Arrange
+        ClosestNumber closestNumber = new();
+
+        // Act
+        var actual = closestNumber.GetClosestPermutation("77", "95");
+
+        // Assert
+        actual.Should().Be("59");
+    }
+
+    [TestMethod]
+    public void GetMinimumDrops_CodingameMoreDigitsWithZero_Returns7400()
+    {
+        // Arrange
+        ClosestNumber closestNumber = new();
+
+        // Act
+        var actual = closestNumber.GetClosestPermutation("8732", "4000007");
+
+        // Assert
+        actual.Should().Be("7400");
+    }
+
+    [TestMethod]
+    [DataRow("7326581367582145004806383665233945377507377600408046588615944865665876581942889334261190569828488903", "3206728551185670554010350791869507515399395992812150593418419992328800823624820469621655802304007277", "7326581367582145004806383665233945377507377600408046588599999999999888555555422222222211111111000000")]
+    public void GetMinimumDrops_CodingameLargeNumbers_PerformsCorrectly(string target, string source, string expected)
+    {
+        // Arrange
+        ClosestNumber closestNumber = new();
+
+        // Act
+        var actual = closestNumber.GetClosestPermutation(target, source);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
 }
