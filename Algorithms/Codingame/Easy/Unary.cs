04b0fdd2240811179422e0de6443cee357701ea9
Hervé BLONDEAU <hblondeau-ext@alpheys.com>
2024-07-11 14:55:54 +0200

    Add Codingame's Unary

---
+++ Algorithms/Codingame/Easy/Unary.cs
@@ -0,0 +1,34 @@
+﻿// https://www.codingame.com/training/easy/unary
+
+using System;
+using System.Text;
+
+namespace Codingame.Easy.Unary;
+
+public class Unary
+{
+    public string Encode(string message)
+    {
+        // Build the binary string representation of the message, for instance "10000111000011" for "CC"
+        byte[] asciiBytes = Encoding.ASCII.GetBytes(message);
+        string binary = "";
+        foreach (var b in asciiBytes) binary += Convert.ToString(b, 2).PadLeft(7, '0');
+
+        // Build the result
+        string result = "";
+        char previous = '\u00ff'; // a random character outside of the default ASCII range
+        foreach (char current in binary)
+        {
+            // New value => build the first block
+            if (current != previous) result += " " + (current == '1' ? "0" : "00") + " ";
+
+            // Append the value to the second block
+            result += "0";
+
+            previous = current;
+        }
+
+        // The generic loop above has inserted a space at the very beginning, which we need to remove
+        return result.TrimStart();
+    }
+}
---
+++ AlgorithmsTests/Codingame/Easy/UnaryTests.cs
@@ -0,0 +1,26 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codingame.Easy.Unary;
+
+[TestClass]
+public class UnaryTests
+{
+    [TestMethod]
+    [DataRow("", "")]
+    [DataRow("C", "0 0 00 0000 0 00")]
+    [DataRow("CC", "0 0 00 0000 0 000 00 0000 0 00")]
+    [DataRow("%", "00 0 0 0 00 00 0 0 00 0 0 0")]
+    [DataRow("Chuck Norris' keyboard has 2 keys: 0 and white space.", "0 0 00 0000 0 0000 00 0 0 0 00 000 0 000 00 0 0 0 00 0 0 000 00 000 0 0000 00 0 0 0 00 0 0 00 00 0 0 0 00 00000 0 0 00 00 0 000 00 0 0 00 00 0 0 0000000 00 00 0 0 00 0 0 000 00 00 0 0 00 0 0 00 00 0 0 0 00 00 0 0000 00 00 0 00 00 0 0 0 00 00 0 000 00 0 0 0 00 00000 0 00 00 0 0 0 00 0 0 0000 00 00 0 0 00 0 0 00000 00 00 0 000 00 000 0 0 00 0 0 00 00 0 0 000000 00 0000 0 0000 00 00 0 0 00 0 0 00 00 00 0 0 00 000 0 0 00 00000 0 00 00 0 0 0 00 000 0 00 00 0000 0 0000 00 00 0 00 00 0 0 0 00 000000 0 00 00 00 0 0 00 00 0 0 00 00000 0 00 00 0 0 0 00 0 0 0000 00 00 0 0 00 0 0 00000 00 00 0 0000 00 00 0 00 00 0 0 000 00 0 0 0 00 00 0 0 00 000000 0 00 00 00000 0 0 00 00000 0 00 00 0000 0 000 00 0 0 000 00 0 0 00 00 00 0 0 00 000 0 0 00 00000 0 000 00 0 0 00000 00 0 0 0 00 000 0 00 00 0 0 0 00 00 0 0000 00 0 0 0 00 00 0 00 00 00 0 0 00 0 0 0 00 0 0 0 00 00000 0 000 00 00 0 00000 00 0000 0 00 00 0000 0 000 00 000 0 0000 00 00 0 0 00 0 0 0 00 0 0 0 00 0 0 000 00 0")]
+    public void Encode_WhenCalled_PerformsCorrectly(string message, string expected)
+    {
+        // Arrange
+        Unary unary = new();
+
+        // Act
+        var actual = unary.Encode(message);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+}
