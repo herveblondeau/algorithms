6c6934aee376616af89b05167d938fa428b6f5c0
Hervé BLONDEAU <hblondeau-ext@alpheys.com>
2024-12-02 20:46:42 +0100

    Add Advent of Code 2024 day 1 part 2

--- Algorithms/AdventOfCode/2024/Day1HistorianHysteria.cs
+++ Algorithms/AdventOfCode/2024/Day1HistorianHysteria.cs
@@ -8,6 +8,15 @@ public class Day1HistorianHysteria
 {
     public int CalculateDistance(List<int> list1, List<int> list2)
     {
-        return list1.Order().Zip(list2.Order(), ((x, y) => Math.Abs(x - y))).Sum();
+        return list1.Order().Zip(list2.Order(), (x, y) => Math.Abs(x - y)).Sum();
+    }
+
+    public int CalculateSimilarity(List<int> list1, List<int> list2)
+    {
+        Dictionary<int, int> list2Occurrences = list2
+            .GroupBy(n => n)
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        return list1.Sum(n => n * (list2Occurrences.ContainsKey(n) ? list2Occurrences[n] : 0));
     }
 }
