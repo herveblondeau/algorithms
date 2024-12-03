79e1470eadfe4cf2fdb094344222b2c5c6b50f21
Hervé BLONDEAU <hblondeau-ext@alpheys.com>
2024-12-03 18:43:24 +0100

    Add Advent of Code 2024 day 3 part 1

---
+++ Algorithms/AdventOfCode/2024/Day3MullItOver.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Algorithms.AdventOfCode._2024;
+
+public class Day3MullItOver
+{
+    public int CalculateMulSum(string input)
+    {
+        Regex regex = new Regex(@"mul\((\d+),(\d+)\)");
+        var matches = regex.Matches(input);
+        return matches.Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value));
+    }
+}
